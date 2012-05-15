d4c33200b308e43e1a9f496a14d3c2dc274c6c79
Andy Rossmeissl <andy@rossmeissl.net>
2012-05-15 14:08:23 -0500

    Stub Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperEnergyApp::Application.config.secret_token = '40b92608b04da0721b4b50485f1ed0f2ec5c892e6583a8099da5acf05bdd520a721334aaceb9fc6e6a2303cd08d5e1a29f1c9563f8868cce260ff19509a99b82'
