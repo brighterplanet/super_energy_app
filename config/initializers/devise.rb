dc06f8e87faaf6e63cd298a9635facc82f29a995
Derek Kastner <dkastner@gmail.com>
2012-05-15 16:52:02 -0400

    Initial Sparkwire oauth setup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require 'sparkwire'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -203,7 +205,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :sparkwire, 'APP_ID', 'APP_SECRET', :scope => 'user'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
---
+++ lib/sparkwire.rb
@@ -0,0 +1,28 @@
+require 'omniauth-oauth'
+
+module OmniAuth
+  module Strategies
+    class Sparkwire < OmniAuth::Strategies::OAuth
+      option :name, 'sparkwire'
+
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, { :site => 'http://super-energy-app.herokuapp.com' }
+
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
+      uid { request.params['user_id'] }
+
+      info do
+        { }
+      end
+
+      extra do
+        { }
+      end
+    end
+  end
+end
