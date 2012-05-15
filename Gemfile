74a6768a4dfe6bc03becf49500f795fdb6ce8398
Andy Rossmeissl <andy@rossmeissl.net>
2012-05-15 16:29:25 -0500

    Show Derek how to do a boring graph

--- Gemfile
+++ Gemfile
@@ -20,3 +20,5 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'uglifier', '>= 1.0.3'
 end
+
+gem 'nokogiri'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
+    doc = Nokogiri::XML(open(Rails.root.join 'doc/sample_data.xml'))
+    doc.remove_namespaces!
+    readings = doc.xpath('//value').map { |v| v.children.first.to_s }.map(&:to_i)
+    readings.pop
+    @readings = (1..readings.length).zip readings
   end
 end
