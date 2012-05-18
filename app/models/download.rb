0f108a9115b3a6b30b60860d3b7f74d23194caaf
Derek Kastner <dkastner@gmail.com>
2012-05-18 13:57:27 -0400

    Fix the case of the missing download

--- app/models/download.rb
+++ app/models/download.rb
@@ -8,6 +8,8 @@ class Download < ActiveRecord::Base
   def self.update(user, token, secret)
     if user.download && user.download.expired?
       user.download.destroy
+    end
+    if user.download.nil?
       user.create_download! :data => Sparkwire.fetch(token, secret)
     end
     user.download
